a30b4fdd95deda8a6ff0b5a355273bf7d306c8bc
quinnjn <quinnjn@gmail.com>
2020-12-04 15:31:37 -0800

    Init commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://lol.secure.dyn.riotcdn.net/channels/public/x/installer/current/live.na.exe'
+$checksum   = '9AC163C08713144D741D2483563D4C74074EECC12159B13FB44AAC2BA9844DEA'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  url           = $url
+  softwareName  = 'League of Legends'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs    = "--skip-to-install"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
